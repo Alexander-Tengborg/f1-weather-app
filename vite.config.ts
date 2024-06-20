2cbe47eadcdc4d23bcab662a4cd015cfd3d80763
Alexander-Tengborg <Landervich@Gmail.com>
2024-06-20 23:48:39 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'f1-weather-app',
   resolve: {
     preserveSymlinks: true
   }
